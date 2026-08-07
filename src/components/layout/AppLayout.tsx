c2c9772e0afdf2e50c02f10357638b7b661b9d27
Izzy678 <128699079+Izzy678@users.noreply.github.com>
2026-08-07 05:45:01 +0100

    conductor-sync-2026-08-07--04-45-01

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "@/components/layout/Header";
+import { AppSidebar } from "@/components/layout/AppSidebar";
 
 export default function AppLayout() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1 container mx-auto px-4 py-8">
-        <Outlet />
-      </main>
+    <div className="relative min-h-screen bg-background">
+      <AppSidebar />
+      <div className="lg:pl-60">
+        <main className="mx-auto w-full max-w-6xl px-4 py-6 sm:px-6 sm:py-8 lg:px-8 lg:py-10">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
